5a2223b4603b410a60086a8d444e435a156b56d5
josemrobles <joserobles@robi.local>
2016-04-30 19:35:20 -0400

    Tried to add a bit more but it's dinner time

--- app/main.go
+++ app/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"flag"
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -11,16 +10,20 @@ func main() {
 
 	host := flag.String("host", "", "MySQL server host")
 	port := flag.String("port", "", "server port")
-	datbase := flag.String("database", "", "default database")
+	database := flag.String("database", "", "default database")
 	query := flag.String("query", "", "a SQL query")
 	query_file := flag.String("query_file", "", "a SQL query file")
 	user := flag.String("user", "", "username")
 	password := flag.String("password", "", "password")
 	flag.Parse()
 
-	con, err := sql.Open("mysql", user+":"+password+"@tcp("+host+":"+port+")/"+database+"?allowOldPasswords=1")
+	c, err := sql.Open("mysql", *user+":"+*password+"@tcp("+*host+":"+*port+")/"+*database+"?allowOldPasswords=1")
 	if err != nil {
 		panic(err)
 	}
-	defer con.Close()
+	defer c.Close()
+}
+
+func getDatai(c *sql.DB, query string) (error, data string) {
+
 }
