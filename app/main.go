adeb7fa4cd877e238e24417c8dd2e54ca33b8bf3
josemrobles <joserobles@robi.local>
2016-04-30 22:09:15 -0400

    getting there

--- app/main.go
+++ app/main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"flag"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
 )
 
 func main() {
 
+	// Flags
 	host := flag.String("host", "", "MySQL server host")
 	port := flag.String("port", "", "server port")
 	database := flag.String("database", "", "default database")
@@ -18,27 +21,63 @@ func main() {
 	password := flag.String("password", "", "password")
 	flag.Parse()
 
+	// Connect to mysql database
 	db, err := sql.Open("mysql", *user+":"+*password+"@tcp("+*host+":"+*port+")/"+*database+"?allowOldPasswords=1")
 	if err != nil {
-		log.Println("one")
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	getData(db, *query)
+	jsonData, err := getData(db, *query)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Print(jsonData)
+	}
 }
 
-func getData(db *sql.DB, query string) {
+func getData(db *sql.DB, query string) (string, error) {
+
+	// Execute the query
 	rows, err := db.Query(query)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 	defer rows.Close()
-	for rows.Next() {
-		// do stuff with data
-	}
-	err = rows.Err()
+
+	// Get the column names
+	columns, err := rows.Columns()
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
+	count := len(columns)
+
+	// Get the data and convert to json
+	tableData := make([]map[string]interface{}, 0)
+	data := make([]interface{}, count)
+	valuePtrs := make([]interface{}, count)
+	for rows.Next() {
+		for i := 0; i < count; i++ {
+			valuePtrs[i] = &data[i]
+		}
+		rows.Scan(valuePtrs...)
+		entry := make(map[string]interface{})
+		for i, column := range columns {
+			var v interface{}
+			datum := data[i]
+			b, ok := datum.([]byte)
+			if ok {
+				v = string(b)
+			} else {
+				v = datum
+			}
+			entry[column] = v
+		}
+		tableData = append(tableData, entry)
+	}
+	json, err := json.Marshal(tableData)
+	if err != nil {
+		return "", err
+	}
+	return string(json), nil
 }
